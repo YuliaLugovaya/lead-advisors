7144c1617ae5512a758ea5632e27439a9c73ef83
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-06-10 20:54:56 +0300

    feat: add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,24 +42,78 @@ function initializeClock(id, endtime) {
 const deadline = "July 24 2023 00:00:00 GMT+0300";
 initializeClock('countdown', deadline);
 
+
+
+
 //form
 
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.footer__item-input');
+  const form = document.querySelector('#form');
+  const input = document.querySelector('#email');
+  const btn = document.querySelector('.footer__input-button');
+  const modalTitle = document.querySelector('.modal-title');
+  const modalText = document.querySelector('.modal-body');
   form.addEventListener('submit', formSend);
+
+  const modal = new bootstrap.Modal(document.querySelector('#exampleModal'));
 
   async function formSend(e) {
     e.preventDefault();
-    let error = formValidate(form);
+
+    if (input.classList.contains('valid')) {
+      try {
+        let response = await fetch("https://httpbin.org/post",
+          {
+            method: 'POST',
+            body: JSON.stringify(input.value),
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8'
+            }
+          });
+
+        let data = await response.json();
+        console.log(data);
+        form.reset();
+        modal.show();
+        modalTitle.innerHTML = `SUCCESS!`
+        modalText.innerHTML = `You have successfully subscribed to the email newsletter!`
+      } catch (error) {
+        console.log(error);
+        modal.show();
+        modalTitle.innerHTML = `ERROR`
+        modalText.innerHTML = `Try again!`
+      }
+    }
   }
 
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelector('._req');
-  }
+  // function formValidate(form) {
+  //   let error = 0;
+  //   formRemoveError(input);
+  //   if (emailTest(input)) {
+  //     formAddError(input)
+  //     error++;
+  //   }
+  //   if (input.classList.contains('valid')) {
+  //     error = 0;
+  //   }
+  //   console.log(error);
+  //   return error;
+
+  // }
+
+  // function formAddError(input) {
+  //   input.parentElement.classList.add('_error');
+  //   input.classList.add('_error');
+  // }
+
+  // function formRemoveError(input) {
+  //   input.parentElement.classList.remove('_error');
+  //   input.classList.remove('_error');
+  // }
+
+  // function emailTest(input) {
+  //   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  // }
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
-});
+
